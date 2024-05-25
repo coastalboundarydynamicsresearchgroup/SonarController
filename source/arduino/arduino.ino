107d248aa399006004f9fe2073e1f58a56a82ce0
Louis Ross <louis.ross@gmail.com>
2024-05-25 22:22:45 +0000

    Rework to act as dumb passthrough

--- source/arduino/arduino.ino
+++ source/arduino/arduino.ino
@@ -1,4 +1,4 @@
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 
 /*
   arduino.ino
@@ -11,7 +11,7 @@
   2. Monitor remote commands coming from Serial1, and pass them to the host through Serial.
 */
 
-unsigned long timeOfLastUpdate = 0;
+unsigned long timeOfLastUpdate = millis();
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -19,17 +19,71 @@ void setup() {
 
   // Serial connects to the Sonar controller, our host.
   Serial.begin(115200);
+  //Serial.setTimeout(10);
 
   // Serial1 connects to a remote command source.
   Serial1.begin(115200);
-  Serial1.setTimeout(10);
+  //Serial1.setTimeout(10);
 
   // We will be blinking the builting LED, just for fun.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
+char buffer[200];
+int gotLinefeed = 0;
+
 // the loop routine runs over and over again forever:
 void loop() {
+  int availableCommand = Serial1.available();
+  if (availableCommand > 0)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    if (availableCommand > 200)
+      availableCommand = 200;
+
+    availableCommand = Serial1.readBytes(buffer, availableCommand);
+    gotLinefeed = buffer[availableCommand - 1] == '\n';
+    Serial.write(buffer, availableCommand);
+  }
+
+  availableCommand = Serial.available();
+  if (availableCommand > 0)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    if (availableCommand > 200)
+      availableCommand = 200;
+    availableCommand = Serial.readBytes(buffer, availableCommand);
+    Serial1.write(buffer, availableCommand);
+  }
+
+  // We want to periodically send voltage and other status, regardless of remote commands.
+  unsigned long now = millis();
+
+  // Do an update if either it is time or we have a remote command.
+  if (((now - timeOfLastUpdate) > 500) && gotLinefeed)
+  {
+    // Blink the builtin LED every time we send status to the host.
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    // Set up for next time.
+    timeOfLastUpdate = now;
+
+    // read the input on analog pin 0:
+    int batteryVoltage = analogRead(A8);
+    int referenceVoltage = analogRead(A9);
+
+    // The JSON object will have one property at least.
+    String command = "{\"Command\":\"Status\",\"batteryVoltage\":";
+    command += String(batteryVoltage, DEC);
+    command += ",\"referenceVoltage\":";
+    command += String(referenceVoltage, DEC);
+    command += "}";
+
+    // Send whatever JSON object we have accumulated to the host.
+    Serial.println(command);
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+/*
   // We want to periodically send voltage and other status, regardless of remote commands.
   unsigned long now = millis();
 
@@ -84,6 +138,6 @@ void loop() {
       Serial1.println(response);
     }
   }
-
   digitalWrite(LED_BUILTIN, LOW);
+*/
 }
